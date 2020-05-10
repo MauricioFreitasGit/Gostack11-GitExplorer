9feaaca69293e3527fd253dadd805aaa845a704e
Mauricio <mauricio@MacBook-Pro-de-User.local>
2020-05-10 17:15:20 -0300

    Primeira versão

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Title } from  './styles';
+
+const Dashboard: React.FC = () => {
+  return <Title>Expore respositorios no github</Title>;
+};
+
+export default Dashboard;
