9b2c1e6ccc961e761ced795fbcd2e99e577a7071
Mauricio <mauricio@MacBook-Pro-de-User.local>
2020-05-11 20:29:46 -0300

    estilizando o projeto

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,57 @@
 import React from 'react';
-import { Title } from  './styles';
+import logoimg from '../../assets/logo.svg';
+import { Title, Form, Repositories } from './styles';
+import { FiChevronRight } from 'react-icons/fi';
+
+import Repository from '../Repository';
 
 const Dashboard: React.FC = () => {
-  return <Title>Expore respositorios no github</Title>;
+  return (
+    <>
+      <img src={logoimg} alt="Github Explorer" />
+      <Title>Expore respositorios no github</Title>
+      <Form action="form">
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit" >Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img src="https://avatars0.githubusercontent.com/u/46382799?s=460&u=60ed2d1401dbd75e793ba7cd847a31c3e7372410&v=4"
+            alt="Mauricio Freitas"
+          />
+            <div>
+              <strong>mapa-ios-react</strong>
+              <p>Esse repositório tem como objetivo instruir a realizar a instalação do Maps do iOS e rodar ele em seu dispositivo</p>
+            </div>
+            <FiChevronRight size={20}/>
+
+        </a>
+        <a href="teste">
+          <img src="https://avatars0.githubusercontent.com/u/46382799?s=460&u=60ed2d1401dbd75e793ba7cd847a31c3e7372410&v=4"
+            alt="Mauricio Freitas"
+          />
+            <div>
+              <strong>mapa-ios-react</strong>
+              <p>Esse repositório tem como objetivo instruir a realizar a instalação do Maps do iOS e rodar ele em seu dispositivo</p>
+            </div>
+            <FiChevronRight size={20}/>
+
+        </a>
+        <a href="teste">
+          <img src="https://avatars0.githubusercontent.com/u/46382799?s=460&u=60ed2d1401dbd75e793ba7cd847a31c3e7372410&v=4"
+            alt="Mauricio Freitas"
+          />
+            <div>
+              <strong>mapa-ios-react</strong>
+              <p>Esse repositório tem como objetivo instruir a realizar a instalação do Maps do iOS e rodar ele em seu dispositivo</p>
+            </div>
+            <FiChevronRight size={20}/>
+
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
