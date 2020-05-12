438759f67f2281871a127b51e0dde39c2a1fe5d2
Mauricio <mauricio@MacBook-Pro-de-User.local>
2020-05-11 21:14:21 -0300

    conectando com api

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,54 +1,61 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import logoimg from '../../assets/logo.svg';
 import { Title, Form, Repositories } from './styles';
 import { FiChevronRight } from 'react-icons/fi';
-
+import api from '../../services/api';
 import Repository from '../Repository';
 
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
 const Dashboard: React.FC = () => {
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+  const [newrepo, setNewrepo] = useState('');
+
+  async function hadleAddRepository(event: FormEvent<HTMLFormElement>): Promise<void> {
+    event.preventDefault();
+
+    const response = await api.get<Repository>(`repos/${newrepo}`);
+
+    const repository = response.data;
+    setRepositories([...repositories, repository]);
+
+    setNewrepo('');
+  }
+
   return (
     <>
       <img src={logoimg} alt="Github Explorer" />
       <Title>Expore respositorios no github</Title>
-      <Form action="form">
-        <input placeholder="Digite o nome do repositório" />
+      <Form action="form" onSubmit={hadleAddRepository}>
+        <input
+          value={newrepo}
+          onChange={(e) => setNewrepo(e.target.value)}
+          placeholder="Digite o nome do repositório"
+        />
         <button type="submit" >Pesquisar</button>
       </Form>
 
       <Repositories>
-        <a href="teste">
-          <img src="https://avatars0.githubusercontent.com/u/46382799?s=460&u=60ed2d1401dbd75e793ba7cd847a31c3e7372410&v=4"
-            alt="Mauricio Freitas"
-          />
+        {repositories.map(repository => (
+          <a key={repository.full_name} href="teste">
+            <img src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
             <div>
-              <strong>mapa-ios-react</strong>
-              <p>Esse repositório tem como objetivo instruir a realizar a instalação do Maps do iOS e rodar ele em seu dispositivo</p>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
             </div>
-            <FiChevronRight size={20}/>
+            <FiChevronRight size={20} />
 
-        </a>
-        <a href="teste">
-          <img src="https://avatars0.githubusercontent.com/u/46382799?s=460&u=60ed2d1401dbd75e793ba7cd847a31c3e7372410&v=4"
-            alt="Mauricio Freitas"
-          />
-            <div>
-              <strong>mapa-ios-react</strong>
-              <p>Esse repositório tem como objetivo instruir a realizar a instalação do Maps do iOS e rodar ele em seu dispositivo</p>
-            </div>
-            <FiChevronRight size={20}/>
-
-        </a>
-        <a href="teste">
-          <img src="https://avatars0.githubusercontent.com/u/46382799?s=460&u=60ed2d1401dbd75e793ba7cd847a31c3e7372410&v=4"
-            alt="Mauricio Freitas"
-          />
-            <div>
-              <strong>mapa-ios-react</strong>
-              <p>Esse repositório tem como objetivo instruir a realizar a instalação do Maps do iOS e rodar ele em seu dispositivo</p>
-            </div>
-            <FiChevronRight size={20}/>
-
-        </a>
+          </a>
+        ))}
       </Repositories>
     </>
   );
